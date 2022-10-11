bf09883571dacdac5502517d11bd5296b0d8f18a
nickadamson <naburner@protonmail.com>
2022-10-10 22:28:15 -0700

    refactor: api route for next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_CONSUMER_KEY as string,
+      clientSecret: process.env.TWITTER_CONSUMER_SECRET as string,
+    }),
+  ],
+});
