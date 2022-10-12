c7a6da1f1f010008a5425a82055a6073fe1f9a79
nickadamson <naburner@protonmail.com>
2022-10-11 17:51:45 -0700

    chore: draft out process for authentication
    edit env template

--- src/pages/api/verify/[signedMessage].ts
+++ src/pages/api/verify/[signedMessage].ts
@@ -1,5 +1,31 @@
-import { ethers } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { ethers } from "ethers";
+
+/** Twitter Verification and Minting Workflow // Draft
+ * ** Client-Side **                    ** Server-Side **
+ * - new user connects wallet
+ * - prompt user to connect Twitter
+ * - user approves our app
+ * -->                                  - api/auth gets twitter details
+ *                                        - implement requirements/checks here,
+ *                                          ex: follower count, account age,
+ *                                          account used already for dif addr.
+ *                                          Latter would req hosting a DB.
+ *                                      - a new 'session' is created, JWT is
+ *                                      - encoded & sent to client
+ * - client is signed in to twitter   <--
+ * - have user sign message of:
+ *   {
+ *      address,
+ *      session JWT
+ *   }
+ * - verify signedMessage was signed
+ *   by correct address
+ * - call api/verify/[address]
+ * -->                                  - verify JWT
+ *                                      - mint Token to address
+ * - sign out
+ *  */
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function handler(
@@ -7,25 +33,35 @@ export default async function handler(
   res: NextApiResponse<{ success: boolean; reason: string }>
 ) {
   const {
-    query: { userAddress, twitterUsername },
+    query: { address },
     method,
   } = req;
 
   switch (method) {
     case "PUT":
-      try {
-        // TODO use private key to mint()
-      } catch (error) {
-        return res.status(500).json({
-          success: false,
-          reason: `Internal Server Error while trying to mint.\n${JSON.stringify(
-            error
-          )}`,
-        });
-      }
-      return res
-        .status(200)
-        .json({ success: true, reason: "Successfully Verified" });
+
+    // check JWT
+
+    // if unauth, return 401 unauthorized
+
+    // if verified, try to mint NFT to user's address
+
+    // TODO use private key to mint()
+
+    // return res.status(200).json({
+    //   success: true,
+    //   reason: "Successfully Verified. Check your wallet for NFT.",
+    // });
+
+    // if minting fails, tell user reason, and log to server
+
+    // return res.status(500).json({
+    //   success: false,
+    //   reason: `Internal Server Error while trying to mint.\n${JSON.stringify(
+    //     error
+    //   )}`,
+    // });
+
     default:
       return res.status(400).json({
         success: false,
