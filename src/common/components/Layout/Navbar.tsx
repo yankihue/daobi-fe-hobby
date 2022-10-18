eaf3db519fd23ed801c1a175d1fd021002f1fa12
nickadamson <naburner@protonmail.com>
2022-10-17 20:10:18 -0700

    refactor: wallet info

--- src/common/components/Layout/Navbar.tsx
+++ src/common/components/Layout/Navbar.tsx
@@ -1,9 +1,12 @@
-import { useContractRead } from "wagmi";
+import { useAccount, useContractRead } from "wagmi";
 import Wallet from "./Wallet";
 import Contract3ABI from "../../../ethereum/abis/DAObiContract3.json";
 import { toTrimmedAddress } from "@/utils/index";
+import { useEffect, useState } from "react";
+import useRoles from "@/hooks/useRoles";
 
 const Navbar = () => {
+  const { address } = useAccount();
   const result = useContractRead({
     address:
       process.env.NEXT_PUBLIC_TOKEN_ADDR ??
@@ -12,24 +15,45 @@ const Navbar = () => {
     functionName: "chancellor",
   });
 
+  const [chancellorAddress, setChancellorAddress] = useState("");
+  useEffect(() => {
+    if (result.data) {
+      setChancellorAddress(result.data as string);
+    }
+  }, [result]);
+
+  const { isChancellor, rolesLoading } = useRoles(address);
+
   return (
-    <div className="flex justify-between place-items-center px-4 w-full max-w-full h-24 border-b">
-      <p className="pl-4 w-1/3 text-left">DAObi</p>
-      <p className="w-1/3 text-center">
-        {!isLoading && !isError && (
-          <>
-            The current Chancellor is:
-            <br />
-            <a href={`https://mumbai.polygonscan.com/address/${data}`}>
-              {toTrimmedAddress(data as unknown as string)}
+    <nav className="flex justify-between justify-self-start items-center px-6 py-4 mb-4 w-full max-w-full h-full max-h-16 border-b border-color-mode">
+      <div className="w-1/3">
+        <h1 className="text-left">DAObi</h1>
+      </div>
+      <div className="flex justify-center items-center mx-auto space-x-3 w-1/3 text-center whitespace-nowrap">
+        <div className="hidden flex-col justify-center items-center lg:flex">
+          <p>
+            Today&#39;s Chancellor is{" "}
+            <a
+              href={`https://mumbai.polygonscan.com/address/${chancellorAddress}`}
+            >
+              {toTrimmedAddress(result.data as unknown as string)}
             </a>
-          </>
-        )}
-      </p>
-      <div className="pr-4 mr-0 w-1/3 text-right">
+          </p>
+          <div className="hidden whitespace-nowrap md:inline">
+            {!rolesLoading &&
+              `${
+                isChancellor
+                  ? "👑 Welcome Chancellor! 🏰"
+                  : "🌾 Maybe One Day... 🛖"
+              }
+            `}
+          </div>
+        </div>
+      </div>
+      <div className="mr-0 w-1/3 text-right">
         <Wallet />
       </div>
-    </div>
+    </nav>
   );
 };
 
