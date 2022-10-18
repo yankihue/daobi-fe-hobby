f3b92c3fd36d7b3806465d4fc9646d302e5c5399
nickadamson <naburner@protonmail.com>
2022-10-17 20:17:10 -0700

    feat: add footer to pageLayout

---
+++ src/common/components/Layout/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-start justify-self-end mt-16 w-full h-32 border-t shadow-lg align-center border-color-mode footer">
+      <div className="flex flex-col m-auto space-y-1 w-full text-center lg:w-1/6">
+        <a className="" href="https://daobi.netlify.app/court/">
+          DAObi Homepage
+        </a>
+        <a className="" href="https://discord.com/invite/HqvVRPZ9Gg">
+          Discord
+        </a>
+        <a className="" href="https://github.com/bluentity/daobi">
+          Contract Source Code
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/common/components/Layout/PageLayout.tsx
+++ src/common/components/Layout/PageLayout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const PageLayout = ({ children }: { children: ReactNode }): JSX.Element => (
-  <div className="flex-col mx-auto w-full h-full min-h-screen min-w-screen">
-    <Navbar />
-    {children}
-  </div>
+  <>
+    <div className="flex flex-col justify-between mx-auto w-full h-full min-h-screen min-w-screen">
+      <div className="flex flex-col">
+        <Navbar />
+        {children}
+      </div>
+      <Footer />
+    </div>
+  </>
 );
 
 export default PageLayout;
