169b66289c157639210c123dab43e69f9c0424ef
nickadamson <naburner@protonmail.com>
2022-10-21 10:09:40 -0700

    chore: plan new landing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from "next";
-
+import { useAccount } from "wagmi";
+import useRoles from "@/hooks/useRoles";
 const Home: NextPage = () => {
-  return <div className="flex flex-col place-self-start w-full h-full"></div>;
+  const { address } = useAccount();
+  const { isVerified, isRegistered } = useRoles(address);
+  return (
+    <div className="flex flex-col place-self-start w-full h-full">
+      {!address &&
+        {
+          /* connect wallet */
+        }}
+      {!isVerified &&
+        {
+          /* verify twitter so we can mint token */
+        }}
+      {!isRegistered &&
+        {
+          /* cast initial vote */
+        }}
+      {isRegistered &&
+        {
+          /* full access contract selection */
+        }}
+    </div>
+  );
 };
 
 export default Home;
